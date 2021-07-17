fe207b9c3a94ec5f1af0e0a50c2d79bac5eee318
oigabrielteodoro <oi@gabrielteodoro.com>
2021-07-16 22:25:48 -0300

    feat: created types to profile, tag, user and article using io-ts

--- src/core/types/article.ts
+++ src/core/types/article.ts
@@ -1,18 +1,22 @@
-import { Tag } from '@/core/types/tag'
-import { Profile } from '@/core/types/profile'
+import * as t from 'io-ts'
 
-export type Article = {
-  slug: string
-  title: string
-  description: string
-  body: string
-  tagList: Tag[]
-  createdAt: string
-  updatedAt: string
-  favorited: boolean
-  favoritesCount: number
-  author: Profile
-}
+import { tagCodec } from '@/core/types/tag'
+import { profileCodec } from '@/core/types/profile'
+
+export const articleCodec = t.type({
+  slug: t.string,
+  title: t.string,
+  description: t.string,
+  body: t.string,
+  tagList: t.array(tagCodec),
+  createdAt: t.string,
+  updatedAt: t.string,
+  favorited: t.boolean,
+  favoritesCount: t.number,
+  author: profileCodec,
+})
+
+export type Article = t.TypeOf<typeof articleCodec>
 
 export type Articles = {
   artigles: Article[]
--- src/core/types/user.ts
+++ src/core/types/user.ts
@@ -1,15 +1,20 @@
-import { Email } from '@/core/types/scalar'
+import * as t from 'io-ts'
+import { emailCodec } from '@/core/types/scalar'
 
-export type User = {
-  email: Email
-  token: string
-  username: string
-  bio: string
-  image: string
-}
+export const userCodec = t.type({
+  email: emailCodec,
+  token: t.string,
+  username: t.string,
+  bio: t.string,
+  image: t.string,
+})
 
-export type CreateUser = {
-  username: string
-  password: string
-  email: Email
-}
+export type User = t.TypeOf<typeof userCodec>
+
+export const createUserCodec = t.type({
+  username: t.string,
+  email: emailCodec,
+  password: t.string,
+})
+
+export type CreateUser = t.TypeOf<typeof createUserCodec>
