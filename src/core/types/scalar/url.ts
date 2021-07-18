a426db7b6d97dc33a211735a6b333701c4566d46
oigabrielteodoro <oi@gabrielteodoro.com>
2021-07-17 23:26:01 -0300

    refactor: isUrl using fp-ts

--- src/core/types/scalar/url.ts
+++ src/core/types/scalar/url.ts
@@ -1,6 +1,8 @@
 import { URL } from 'url'
 import * as t from 'io-ts'
 import { withMessage } from 'io-ts-types'
+import { pipe, constFalse, constTrue } from 'fp-ts/function'
+import * as E from 'fp-ts/Either'
 
 type UrlBrand = {
   readonly Url: unique symbol
@@ -18,10 +20,11 @@ export const urlCodec = withMessage(
 export type Url = t.TypeOf<typeof urlCodec>
 
 function isUrl (value: unknown) {
-  try {
-    const url = new URL(typeof value === 'string' ? value : '')
-    return !!url
-  } catch {
-    return false
-  }
+  return pipe(
+    E.tryCatch(
+      () => new URL(typeof value === 'string' ? value : ''),
+      E.toError,
+    ),
+    E.fold(constFalse, constTrue),
+  )
 }
